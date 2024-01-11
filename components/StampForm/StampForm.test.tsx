f0d44f06e05d6888ec3dd94a63a9c360a011bed2
sakura-adachi <sakura-adachi@cybozu.co.jp>
2024-01-11 21:59:50 +0900

    fix: add fakeTimer for animation delay

--- components/StampForm/StampForm.test.tsx
+++ components/StampForm/StampForm.test.tsx
@@ -1,12 +1,20 @@
 import Providers from "@/libs/providers";
 import renderer from "react-test-renderer";
 import { StampForm } from "./StampForm";
+
+jest.useFakeTimers();
+
 describe("<StampForm />", () => {
   test("if renders", () => {
     const snapshot = renderer
       .create(
         <Providers colorScheme={"light"}>
-          <StampForm />
+          <StampForm
+            user={{ name: "Blah" }}
+            currentDay={2}
+            buttonLabel={"送る"}
+            isLastDay={false}
+          />
         </Providers>,
       )
       .toJSON();
