dc7635b05b3d428dcdc30a72278312b41e710610
sakura-adachi <sakura-adachi@cybozu.co.jp>
2024-01-13 00:11:21 +0900

    chore: sizable wrapper

--- components/StampWrapper/StampWrapper.tsx
+++ components/StampWrapper/StampWrapper.tsx
@@ -4,19 +4,21 @@ import { Typography } from "../Typography/Typography";
 export type StampWrapperProps = {
   stamp?: string;
   isSelector?: boolean;
+  size?: "large" | "small";
 } & SquareProps;
 
 export const StampWrapper = ({
   stamp = "🌟",
   isSelector = false,
+  size = "large",
   ...props
 }: StampWrapperProps) => {
   return (
     <Square
       backgroundColor="rgba(232, 230, 227, 0.52)"
-      width={70}
-      height={70}
-      padding={10}
+      width={size === "small" ? 50 : 70}
+      height={size === "small" ? 50 : 70}
+      padding={size === "small" ? 5 : 10}
       borderRadius={6}
       animation={isSelector ? "bouncy" : ""}
       scale={isSelector ? 0.9 : 1}
@@ -24,7 +26,7 @@ export const StampWrapper = ({
       pressStyle={isSelector ? { scale: 0.925 } : {}}
       {...props}
     >
-      <Typography type="h2">{stamp}</Typography>
+      <Typography type={size === "small" ? "h3" : "h2"}>{stamp}</Typography>
     </Square>
   );
 };
