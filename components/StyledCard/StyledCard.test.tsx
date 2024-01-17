57ae9d581b9fdd66108f031fe93201fcbaae6f0b
sakura-adachi <sakura-adachi@cybozu.co.jp>
2024-01-17 12:45:25 +0900

    fix: animation系テストにタイマーを仕込む

--- components/StyledCard/StyledCard.test.tsx
+++ components/StyledCard/StyledCard.test.tsx
@@ -1,16 +1,19 @@
-import renderer from "react-test-renderer";
+import renderer, { act } from "react-test-renderer";
 import { TamaguiProvider } from "tamagui";
 import config from "../../tamagui.config";
 import { StyledCard } from "./StyledCard";
+jest.useFakeTimers();
 describe("<StyledCard />", () => {
   test("if renders", () => {
-    const snapshot = renderer
-      .create(
-        <TamaguiProvider config={config}>
-          <StyledCard.Card />
-        </TamaguiProvider>,
-      )
-      .toJSON();
+    const snapshot = act(() =>
+      renderer
+        .create(
+          <TamaguiProvider config={config}>
+            <StyledCard.Card />
+          </TamaguiProvider>,
+        )
+        .toJSON(),
+    );
     expect(snapshot).toMatchSnapshot();
   });
 });
