a34874f52606b0e9cd8023e78777141406a526d7
sakura-adachi <sakura-adachi@cybozu.co.jp>
2024-01-18 17:55:36 +0900

    fix: Date固定

--- components/DatePicker/DatePicker.test.tsx
+++ components/DatePicker/DatePicker.test.tsx
@@ -1,6 +1,10 @@
 import Providers from "@/libs/providers";
 import renderer from "react-test-renderer";
 import { DatePicker } from "./DatePicker";
+jest.useFakeTimers();
+const mockDate = new Date("2024/01/18");
+jest.setSystemTime(mockDate);
+
 describe("<DatePicker />", () => {
   test("if renders", () => {
     const snapshot = renderer
