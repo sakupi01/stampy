c3b855d9b65a1aa01ce00cda7dcaee2dfbcfeab5
sakura-adachi <sakura-adachi@cybozu.co.jp>
2024-01-24 15:40:48 +0900

    fix: fix Date

--- ui/CreateCardForm/CreateCardForm.test.tsx
+++ ui/CreateCardForm/CreateCardForm.test.tsx
@@ -1,6 +1,11 @@
 import Providers from "@/libs/providers";
 import renderer from "react-test-renderer";
 import { CreateCardForm } from "./CreateCardForm";
+
+jest.useFakeTimers();
+const mockDate = new Date("2024/01/18");
+jest.setSystemTime(mockDate);
+
 describe("<CreateCardForm />", () => {
   test("if renders", () => {
     const snapshot = renderer
