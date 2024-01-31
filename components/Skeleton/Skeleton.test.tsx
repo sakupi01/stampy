9400075202e5414c6a3da2b162a87d3ea9b584b5
sakura-adachi <sakura-adachi@cybozu.co.jp>
2024-01-31 12:06:22 +0900

    chore: スナップショットシリアライザをカスタムして特定の属性をスナップショット対象から外す

--- components/Skeleton/Skeleton.test.tsx
+++ components/Skeleton/Skeleton.test.tsx
@@ -1,6 +1,18 @@
 import Providers from "@/libs/provider/providers";
 import renderer from "react-test-renderer";
 import { ListSkeleton } from "./Skeleton";
+expect.addSnapshotSerializer({
+  test: (val) =>
+    val?.props && (val.props.clipPath || val.props.fill || val.props.name),
+  print: (val, serialize, indent) =>
+    `${val.type} ${Object.keys(val.props)
+      .filter(
+        (propName) =>
+          propName !== "clipPath" && propName !== "fill" && propName !== "name",
+      )
+      .map((propName) => `${propName}=${serialize(val.props[propName])}`)
+      .join(" ")}`,
+});
 describe("<Skeleton />", () => {
   test("if renders", () => {
     const snapshot = renderer
--- ui/Lists/LetterList/LetterList.test.tsx
+++ ui/Lists/LetterList/LetterList.test.tsx
@@ -1,6 +1,18 @@
 import Providers from "@/libs/provider/providers";
 import renderer from "react-test-renderer";
 import { LetterList } from "./LetterList";
+expect.addSnapshotSerializer({
+  test: (val) =>
+    val?.props && (val.props.clipPath || val.props.fill || val.props.name),
+  print: (val, serialize, indent) =>
+    `${val.type} ${Object.keys(val.props)
+      .filter(
+        (propName) =>
+          propName !== "clipPath" && propName !== "fill" && propName !== "name",
+      )
+      .map((propName) => `${propName}=${serialize(val.props[propName])}`)
+      .join(" ")}`,
+});
 describe("<LetterList />", () => {
   test("if renders", () => {
     const snapshot = renderer
