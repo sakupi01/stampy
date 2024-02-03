6189c6e1b9b193e0fa537a5ff916f2e9bd12bfc1
sakura-adachi <sakura-adachi@cybozu.co.jp>
2024-02-03 22:54:51 +0900

    refactor: imagePickerの共通化

---
+++ components/libs/imagePicker.ts
@@ -0,0 +1,44 @@
+import { AccountSettingsType } from "@/schema/accountSetting";
+import { StampCardFormType } from "@/schema/stampCard";
+import { useToastController } from "@tamagui/toast";
+import * as ImagePicker from "expo-image-picker";
+import { FieldPath, UseFormSetValue } from "react-hook-form";
+import { assertTruthy } from "../../libs/assertTruthy";
+import { uploadToFirebase } from "../../libs/firebase/upload-image";
+
+type ToastContextI = ReturnType<typeof useToastController>;
+type AllowedFormType = AccountSettingsType | StampCardFormType;
+
+export const pickImage = async <FieldType extends AllowedFormType>({
+  setImage,
+  aspect = [1, 1],
+  toast,
+  setValue,
+  rfhKey,
+}: {
+  setImage: (value: React.SetStateAction<string>) => void;
+  aspect: [number, number];
+  toast: ToastContextI;
+  setValue: UseFormSetValue<FieldType>;
+  rfhKey: FieldPath<FieldType>;
+}) => {
+  // No permissions request is necessary for launching the image library
+  const result = await ImagePicker.launchImageLibraryAsync({
+    mediaTypes: ImagePicker.MediaTypeOptions.All,
+    allowsEditing: true,
+    aspect: aspect,
+    quality: 1,
+  });
+  if (!result.canceled) {
+    const { uri } = result.assets[0];
+    const fileName = uri.split("/").pop();
+    assertTruthy(fileName);
+    const uploadResult = await uploadToFirebase(uri, fileName, "avatar", (v) =>
+      console.log(v),
+    );
+    setImage(uploadResult.downloadUrl);
+    // @ts-ignore
+    setValue(rfhKey, uploadResult.downloadUrl);
+    toast.show("🌟 画像がアップロードされました");
+  }
+};
