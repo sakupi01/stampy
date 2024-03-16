1fe435789d34bdfb20ec2c32d0f95032762beef1
saku-1101 <sakupi1101@outlook.jp>
2024-03-16 20:45:53 +0900

    chore: easbuildのトライ

--- schema/message.ts
+++ schema/message.ts
@@ -1,8 +1,10 @@
 import { Output, object } from "valibot";
 import { messageSchema, stampSchema } from "./schema";
 const MessageFormSchema = object({
+  // carId: cardIdSchema,
   stamp: stampSchema,
   message: messageSchema,
+  // nthDay: dateShema,
 });
 
 type MessageFormType = Output<typeof MessageFormSchema>;
