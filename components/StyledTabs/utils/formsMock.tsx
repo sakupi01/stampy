0a4303c2a0d2fed1ba0fd4a3ddff2200e69cc27f
saku-1101 <sakupi1101@outlook.jp>
2024-03-18 23:45:31 +0900

    feat: 認証繋ぎこみ

--- components/StyledTabs/utils/formsMock.tsx
+++ components/StyledTabs/utils/formsMock.tsx
@@ -3,7 +3,6 @@ import { StyledInput } from "@/components/StyledInput";
 import { Typography } from "@/components/Typography";
 import { authActions } from "@/libs/AsyncStorage/Auth/slice";
 import { signIn, signUp } from "@/libs/auth";
-import { sleep } from "@/libs/sleep";
 import { SignInFormSchema, SignInFormType } from "@/schema/signIn";
 import { SignUpFormSchema, SignUpFormType } from "@/schema/signUp";
 import { valibotResolver } from "@hookform/resolvers/valibot";
@@ -49,16 +48,25 @@ export function SignInForm() {
           console.log("Submitted! :", data);
           dispatch(authActions.isLoading(true));
           // authorization logic with server
-          const { sessionId, user } = await signIn(data);
-          await sleep(1000);
-          // async dispatch so need to wait
-          dispatch(authActions.authorize({ session: sessionId, user: user }));
-          dispatch(authActions.isLoading(false));
-          // clear submitting state
-          reset();
-          // Navigate after signing in. You may want to tweak this to ensure sign-in is
-          // successful before navigating.
-          router.replace("/");
+          const res = await signIn(data);
+          console.log("res:", res);
+
+          if (res.ok) {
+            // // async dispatch so need to wait
+            // set user to redux
+            dispatch(
+              authActions.authorize({
+                token: res.val.token,
+                user: res.val.user,
+              }),
+            );
+            dispatch(authActions.isLoading(false));
+            // clear submitting state
+            reset();
+            // Navigate after signing in. You may want to tweak this to ensure sign-in is
+            // successful before navigating.
+            router.replace("/");
+          }
         })}
       >
         <Typography type="ui" color="$text--subtle">
@@ -151,15 +159,23 @@ export function SignUpForm() {
           console.log("Submitted! :", data);
           dispatch(authActions.isLoading(true));
           // authorization logic with server
-          const { sessionId, user } = await signUp(data);
-          // async dispatch so need to wait
-          dispatch(authActions.authorize({ session: sessionId, user: user }));
-          dispatch(authActions.isLoading(false));
-          // clear submitting state
-          reset();
-          // Navigate after signing in. You may want to tweak this to ensure sign-in is
-          // successful before navigating.
-          router.replace("/");
+          const res = await signUp(data);
+          if (res.ok) {
+            // // async dispatch so need to wait
+            // set user to redux
+            dispatch(
+              authActions.authorize({
+                token: res.val.token,
+                user: res.val.user,
+              }),
+            );
+            dispatch(authActions.isLoading(false));
+            // clear submitting state
+            reset();
+            // Navigate after signing in. You may want to tweak this to ensure sign-in is
+            // successful before navigating.
+            router.replace("/");
+          }
         })}
       >
         <Typography type="ui" color="$text--subtle">
--- libs/auth.ts
+++ libs/auth.ts
@@ -1,18 +1,26 @@
+import { Err, Ok } from "@/repository/result";
 import { SignInFormType } from "@/schema/signIn";
 import { SignUpFormType } from "@/schema/signUp";
 import { User } from "@/types";
 import * as Crypto from "expo-crypto";
 
-async function getUser(token: string): Promise<User | undefined> {
+async function getUser(token: string): Promise<Err<Error> | Ok<User>> {
   // get user info by token
-  // /get/user
-  const res = {
-    id: "",
-    username: "",
-    email: "",
-    avatarUrl: "",
-  };
-  return res;
+  // /user
+  // const repository = new Repository();
+  // const res = await repository.get("/user", false);
+  const mockres = {
+    ok: true,
+    val: {
+      id: "1",
+      username: "saku",
+      email: "saku@mail.com",
+      avatarUrl:
+        "https://images.unsplash.com/photo-1531384441138-2736e62e0919?&w=100&h=100&dpr=2&q=80",
+    },
+    err: null,
+  } as Ok<User>;
+  return mockres;
 }
 
 async function registerUser({
@@ -20,13 +28,23 @@ async function registerUser({
   email,
   hashedPassword,
 }: { username: string; email: string; hashedPassword: string }): Promise<
-  { token: string } | undefined
+  Err<Error> | Ok<{ token: string }>
 > {
   // register and return user from database
   // /signup
+  // const repository = new Repository();
+  // const res = await repository.post(
+  //   "/signup",
+  //   JSON.stringify({ username, email, hashedPassword }),
+  //   false,
+  // );
   const res = {
-    token: "123-xxxxx",
-  };
+    ok: true,
+    val: {
+      token: "123-xxxxx",
+    },
+    err: null,
+  } as Ok<{ token: string }>;
   return res;
 }
 
@@ -34,70 +52,104 @@ async function login({
   email,
   hashedPassword,
 }: { email: string; hashedPassword: string }): Promise<
-  { token: string } | undefined
+  Err<Error> | Ok<{ token: string }>
 > {
   // get user from database
   // /login
+  // const repository = new Repository();
+  // const res = await repository.post(
+  //   "/login",
+  //   JSON.stringify({ email, hashedPassword }),
+  //   false,
+  // );
   const res = {
-    token: "123-xxxxx",
-  };
+    ok: true,
+    val: {
+      token: "123-xxxxx",
+    },
+    err: null,
+  } as Ok<{ token: string }>;
   return res;
 }
 
-async function checkUser(email: string): Promise<boolean | undefined> {
+async function checkUser(
+  email: string,
+): Promise<Err<Error> | Ok<{ doesUserExist: boolean }>> {
   // get user from database
   // /check-user
-  const res = { doesUserExist: false };
-  return res.doesUserExist;
+  // const repository = new Repository();
+  // const res = await repository.put(
+  //   "/check-user",
+  //   JSON.stringify({ email }),
+  //   false,
+  // );
+  const res = {
+    ok: true,
+    val: { doesUserExist: false },
+    err: null,
+  } as Ok<{ doesUserExist: boolean }>;
+
+  if (!res.ok) {
+    return res;
+  }
+
+  return res;
 }
 
 async function signIn(credentials: SignInFormType) {
-  try {
-    const { email, password } = credentials;
-    const hashedPassword = await Crypto.digestStringAsync(
-      Crypto.CryptoDigestAlgorithm.SHA256,
-      password,
-    );
-    const res = await login({ email, hashedPassword });
-    if (!res) {
-      throw new Error("ユーザが登録されていません");
-    }
-    const data = await getUser(res.token);
-    if (!data) {
-      throw new Error("ユーザ情報の取得に失敗しました");
-    }
-    return data;
-  } catch (error: unknown) {
-    throw new Error("Internal Server Error");
+  const { email, password } = credentials;
+  const hashedPassword = await Crypto.digestStringAsync(
+    Crypto.CryptoDigestAlgorithm.SHA256,
+    password,
+  );
+  const res = await login({ email, hashedPassword });
+  if (!res.ok) {
+    return res;
   }
+  const data = await getUser(res.val.token);
+  if (!data.ok) {
+    return data;
+  }
+  return {
+    ok: true,
+    val: { token: res.val.token, user: data.val },
+    err: null,
+  };
 }
 
 async function signUp(credentials: SignUpFormType) {
-  try {
-    const { username, email, password } = credentials;
+  const { username, email, password } = credentials;
 
-    const doesUserExist = await checkUser(username);
-    if (doesUserExist) {
-      throw new Error("このメールアドレスのユーザは既に登録されています.");
-      // -> to sign in
-    }
-
-    const hashedPassword = await Crypto.digestStringAsync(
-      Crypto.CryptoDigestAlgorithm.SHA256,
-      password,
-    );
-    const res = await registerUser({ username, email, hashedPassword });
-    if (!res) {
-      throw new Error("ユーザの登録に失敗しました");
-    }
-    const data = await getUser(res.token);
-    if (!data) {
-      throw new Error("ユーザ情報の取得に失敗しました");
-    }
-    return data;
-  } catch (error: unknown) {
-    throw new Error("Internal Server Error");
+  const doesUserExist = await checkUser(username);
+  if (!doesUserExist.ok) {
+    return doesUserExist;
   }
+  if (doesUserExist.val.doesUserExist) {
+    // -> to sign in
+    return {
+      ok: false,
+      val: null,
+      err: new Error("User already exists"),
+    } as Err<Error>;
+  }
+
+  const hashedPassword = await Crypto.digestStringAsync(
+    Crypto.CryptoDigestAlgorithm.SHA256,
+    password,
+  );
+  const res = await registerUser({ username, email, hashedPassword });
+  if (!res.ok) {
+    return res;
+  }
+  const data = await getUser(res.val.token);
+  if (!data.ok) {
+    return data;
+  }
+  return {
+    ok: true,
+    val: { token: res.val.token, user: data.val },
+    err: null,
+  };
 }
 
 export { signIn, signUp };
