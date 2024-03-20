5b3a14fa68c2f3b40646c3ff7cdc4090cbb675e4
saku-1101 <sakupi1101@outlook.jp>
2024-03-20 10:54:15 +0900

    chore: list sliceを完全消去

--- libs/AsyncStorage/List/state.ts
+++
@@ -1,7 +0,0 @@
-import { createSelector } from "@reduxjs/toolkit";
-import { RootState } from "../store";
-
-export const getListState = createSelector(
-  (state: RootState) => state.list,
-  (list) => list,
-);
